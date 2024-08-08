922602dfbbf0cab718542078b91e437e1d528644
doisego <disidias@gmail.com>
2024-08-08 17:04:03 -0300

    Aula curso 2 Django: Persistencia de Dados e Admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from galeria.models import Fotografia
+
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda", "descricao", "publicada")
+    list_display_links = ("nome", "legenda")
+    list_filter = ("nome", "legenda", "categoria")
+    search_fields = ("nome", "legenda")
+    list_editable = ("publicada",)
+    
 
 # Register your models here.
+admin.site.register(Fotografia, ListandoFotografias)
